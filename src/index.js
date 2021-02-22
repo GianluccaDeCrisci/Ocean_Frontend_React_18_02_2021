e61dac2f8a7f19599749d86a06c71f9704576d71
GianluccaDeCrisci <gian.crisci@gmail.com>
2021-02-22 17:09:58 -0400

    Criação do div Board

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
+function Board() {
+  return <div>Board</div>
+}
+
 // Criação do componente Game
-function Game(){
-  return <div>Jogo</div>
+function Game() {
+  return ( 
+    <div className='game'>
+      <div className='game-board'>
+        <Board/>
+      </div>
+
+      <div className='game-info'>
+        Info
+      </div>
+
+    </div>
+  );
 };
 
 ReactDOM.render(
